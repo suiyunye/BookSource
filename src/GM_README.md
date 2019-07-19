fb38b6c697bd99e73be657e0d6d6a11b7da57832
AngUOI <2503894740@qq.com>
2019-07-19 19:06:52 +0800

    Merge more BookSource

---
+++ src/GM_README.md
@@ -0,0 +1,16 @@
+# 如何使用
+保证网络畅通，运行 `houmo_Bookmerge.exe`，最终会生成`./out/README.md`
+
+# 如何配置
+`hm.json` 用于配置所获取的仓库，以及相关信息
+```json
+{
+        "code": "9251",//暗码
+        "url": "https://gitee.com/halloboy/BookSourceRepository/raw/master/repository.json",//仓库raw地址
+        "author": "Guimi",//作者名
+    }
+```
+## 已收录暗码
+2333、9713、9251、1212、2000
+
+`main.go` 为 `houmo_Bookmerge.exe` 的源代码
---
+++ src/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/tidwall/gjson"
+)
+
+func main() {
+
+	// bookMerge, _ := ioutil.ReadFile("./src/hm.json") 编译前用
+	bookMerge, _ := ioutil.ReadFile("hm.json")
+	lists := gjson.Get(string(bookMerge), "list")
+	time := time.Now().Format("2006-01-02 15:04:05")
+	nums := 0
+	file := "|书源|API|类型|排行榜|账号|版本|暗码|作者|\n| :----------: | ----------------------------------------- | :---: | :----: | :---: | :---: | :---: |:---: |"
+	println(file)
+	for _, lis := range lists.Array() {
+		author := gjson.Get(lis.String(), "author").String()
+		code := gjson.Get(lis.String(), "code").String()
+		burl := gjson.Get(lis.String(), "url").String()
+		rep, _ := http.Get(burl)
+		json, _ := ioutil.ReadAll(rep.Body)
+		list := gjson.Get(string(json), "list")
+		for _, lit := range list.Array() {
+			name := gjson.Get(lit.String(), "name").String()
+			url := gjson.Get(lit.String(), "url").String()
+			category := gjson.Get(lit.String(), "category").String()
+			rank := gjson.Get(lit.String(), "rank").String()
+			auth := gjson.Get(lit.String(), "auth").String()
+			version := gjson.Get(lit.String(), "version").String()
+			allinfo := "|" + name + "|" + url + "|" + category + "|" + rank + "|" + auth + "|" + version + "|" + author + "|" + code + "|"
+			file = file + "\n" + allinfo
+			println(allinfo)
+			nums++
+		}
+	}
+	file = "### 厚墨公共书源集中索引\n" + "#### 更新时间\n" + time + "\n" + "#### 已收录\n" + strconv.Itoa(nums) + "\n\n" + file
+	os.MkdirAll("./out", os.ModePerm)
+	ioutil.WriteFile("./out/README.md", []byte(file), 0640)
+	println("已输出到 ./out/README.md")
+}
